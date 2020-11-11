0c28fe897e17869fd097eded0e4d8521d02e31e9
nulogicsystems <james@nulogicsystems.com>
2020-11-11 00:01:20 -0500

    Modifications
    SystemTray: Hide Yakuake
    version bump

--- usr/share/plasma/look-and-feel/org.kde.blackbird-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
+++ usr/share/plasma/look-and-feel/org.kde.blackbird-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.private.systemtray.js
@@ -1,6 +1,6 @@
 applet.currentConfigGroup = ["General"]
-applet.writeConfig("extraItems","org.kde.plasma.battery,org.kde.plasma.bluetooth,org.kde.plasma.keyboardindicator,org.kde.plasma.printmanager,org.kde.plasma.clipboard,org.kde.plasma.networkmanagement,org.kde.plasma.devicenotifier,org.kde.plasma.diskquota,org.kde.plasma.nightcolorcontrol,KSysTrayCmd,Cadence")
+applet.writeConfig("extraItems","org.kde.plasma.battery,org.kde.plasma.bluetooth,org.kde.plasma.keyboardindicator,org.kde.plasma.printmanager,org.kde.plasma.clipboard,org.kde.plasma.networkmanagement,org.kde.plasma.devicenotifier,org.kde.plasma.diskquota,org.kde.plasma.nightcolorcontrol,KSysTrayCmd,Cadence,Yakuake")
 applet.writeConfig("knownItems","org.kde.plasma.notifications,org.kde.plasma.battery,org.kde.plasma.volume,org.kde.plasma.bluetooth,org.kde.discovernotifier,org.kde.plasma.keyboardindicator,org.kde.kdeconnect,org.kde.plasma.printmanager,org.kde.plasma.clipboard,org.kde.plasma.volumewin7mixer,org.kde.plasma.networkmanagement,org.kde.plasma.devicenotifier,org.kde.plasma.mediacontroller")
-applet.writeConfig("hiddenItems","org.kde.plasma.clipboard,org.kde.plasma.volumewin7mixer,KSysTrayCmd")
+applet.writeConfig("hiddenItems","org.kde.plasma.clipboard,org.kde.plasma.volumewin7mixer,KSysTrayCmd,Yakuake")
 applet.writeConfig("shownItems", "transmission,vokoscreen,yarock,Cadence")
 applet.reloadConfig()
