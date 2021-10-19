6a1710a33b53783dc3e69b652c64e0e6310beeb7
James Kittsmiller <james@nulogicsystems.com>
2021-10-19 13:08:42 -0400

    Updates
    replace ksysguard with plasma-systemmonitor
    
    remove ksystraycmd

--- usr/share/plasma/look-and-feel/org.kde.blackbird-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
+++ usr/share/plasma/look-and-feel/org.kde.blackbird-black.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kicker.js
@@ -3,7 +3,7 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("customButtonImage", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favoriteApps", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "org.kde.discover", "octopi.desktop"])
+applet.writeConfig("favoriteApps", ["org.kde.plasma-systemmonitor.desktop", "systemsettings.desktop", "org.kde.discover", "octopi.desktop"])
 applet.writeConfig("limitDepth", true)
 applet.writeConfig("useCustomButtonImage", true)
 applet.writeConfig("alphaSort", true)
